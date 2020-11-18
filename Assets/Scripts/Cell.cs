3e80d13103c2a41b077bd4f1cd2e17ae54cf3a99
Atharva Bhange <atharva.bhange@gmail.com>
2020-11-18 14:36:40 +0530

    Wall Generation On Start complete

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool [] walls = {true, true, true, true}; // N , E , S , W
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,6 +8,7 @@ public class GridManager : MonoBehaviour
     [Range(1, 50)][SerializeField] int rows = 20;
     [Range(1, 50)][SerializeField] int cols = 20;
     [SerializeField] float blockSize = 1;
+    private Cell cellComponent;
     void Start()
     {
         GenerateGrid();
@@ -15,16 +16,54 @@ public class GridManager : MonoBehaviour
 
 	private void GenerateGrid()
 	{
-        GameObject referenceBlock = (GameObject)Instantiate(Resources.Load("Block"));
+        GameObject referenceCell = (GameObject)Instantiate(Resources.Load("Cell"));
+        GameObject referenceWall = (GameObject)Instantiate(Resources.Load("Wall"));
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++)
             {
-                GameObject block = (GameObject)Instantiate(referenceBlock, transform);
+                GameObject cell = Instantiate(referenceCell, transform);
+                cellComponent = cell.GetComponent<Cell>();
                 float zPos = col * blockSize;
                 float xPos = row * blockSize;
-                block.transform.position = new Vector3(xPos , 0f , zPos);
+                cell.transform.position = new Vector3(xPos , 0f , zPos);
+                
+                DrawWalls(referenceWall, cell);
             }
         }
-        Destroy(referenceBlock);
+        Destroy(referenceCell);
+        Destroy(referenceWall);
 	}
+
+	private void DrawWalls(GameObject referenceWall, GameObject cell)
+	{
+        bool [] wall = cellComponent.walls;
+        if (wall[0]) {
+            var parentPosition = cell.transform.position;
+            GameObject newWall = Instantiate(referenceWall, cell.transform);
+            newWall.transform.position = new Vector3(parentPosition.x ,parentPosition.y + 1.5f , parentPosition.z + 0.5f);
+            newWall.transform.Rotate(0 ,90,0);
+        }
+
+        if (wall[1]) {
+            var parentPosition = cell.transform.position;
+            GameObject newWall = Instantiate(referenceWall, cell.transform);
+            newWall.transform.position = new Vector3(parentPosition.x+0.5f, parentPosition.y + 1.5f, parentPosition.z);
+            newWall.transform.Rotate(0, 180, 0);
+        }
+
+        if (wall[2])
+        {
+            var parentPosition = cell.transform.position;
+            GameObject newWall = Instantiate(referenceWall, cell.transform);
+            newWall.transform.position = new Vector3(parentPosition.x, parentPosition.y + 1.5f, parentPosition.z - 0.5f);
+            newWall.transform.Rotate(0, 270, 0);
+        }
+        if (wall[3])
+        {
+            var parentPosition = cell.transform.position;
+            GameObject newWall = Instantiate(referenceWall, cell.transform);
+            newWall.transform.position = new Vector3(parentPosition.x-0.5f, parentPosition.y + 1.5f, parentPosition.z);
+            newWall.transform.Rotate(0, 180, 0);
+        }
+    }
 }
